693f736e397d388dafde631c8731d615ef57baa8
Adil <adilk676@gmail.com>
2023-08-17 15:45:58 +0100

    initialised git repository

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Footer from "./components/Footer.jsx";
+import Nav from "./components/Nav.jsx";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo.jsx";
+import { useState, useEffect } from "react";
+import Cart from "./pages/Cart.jsx";
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book]);
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => <BookInfo books={books} addToCart={addToCart} />}
+        />
+        <Route path="/cart" render={() => <Cart books={books}/>} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
